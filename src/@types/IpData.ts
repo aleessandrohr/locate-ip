2c25f9a67aae519c15afce3a5f0a7fc1be03aea6
Alessandro Ramos <toprime13@gmail.com>
2021-01-27 14:58:29 -0300

    create type IpData

---
+++ src/@types/IpData.ts
@@ -0,0 +1,13 @@
+export interface IpData {
+  ip: string;
+  city: string;
+  region: string;
+  country_capital: string;
+  country_name: string;
+  latitude: number;
+  longitude: number;
+  timezone: string;
+  org: string;
+  postal: string;
+  version: string;
+}
