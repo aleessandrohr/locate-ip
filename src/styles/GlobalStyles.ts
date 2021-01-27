555effdd3709399a78c9189f923b6e552e684847
Alessandro Ramos <toprime13@gmail.com>
2021-01-27 15:18:42 -0300

    update GlobalStyles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   * {
@@ -9,10 +9,7 @@ export default createGlobalStyle`
 
   html {
     font-size: 16px;
-    min-height: 100%;
-    background-image: url('https://images.pexels.com/photos/240834/pexels-photo-240834.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-    background-size: cover;
-    background-position: center;
+    font-family: "Roboto", sans-serif;
   }
 
 
